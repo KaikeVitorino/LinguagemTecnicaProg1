1a15f4978ebd29a0a31dee7bf8b41da4afcfec92
Kaike.Vitorino <kaike.vitorino@sempreceub.com>
2023-07-31 10:52:46 -0300

    sala1

---
+++ LoopPython1/ConverterTipos.py
@@ -0,0 +1,6 @@
+x=0
+y=1.5
+print(type(x))
+x= int(input('Digite algo '))
+print(x*3)
+print(type(x))
---
+++ LoopPython1/Tipo&Booleano.py
@@ -0,0 +1,27 @@
+#Criando variavel
+x=0
+y=1.5
+l=(1, 3, 4)
+a = 'a'
+b = 'b'
+c = 'quirino'
+list= ['pedro', 'joaquim']
+
+#Printando o tipoda variavel
+print(type(l))
+print(type(a))
+print(type(b))
+print(type(list))
+
+#Print com teste de loop
+print(b+c)
+for i in c:
+    print(i)
+
+#Print com teste de loop em while
+i = 0
+while True:
+    i = i + 1
+    print('teste de loop')
+    if i > 4:
+        break
